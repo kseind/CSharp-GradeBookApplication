9dfd10cd09fd9e9bdcafacab72bb786a81394a3d
k <k>
2018-10-05 18:13:13 +0300

    test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -49,7 +49,7 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             else
             {
-                base.CalculateStudentStatistics();
+                base.CalculateStudentStatistics(name);
             }
         }
     }
