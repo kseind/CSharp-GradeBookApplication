e4d27b7bdc36097c60d8a139b392db31fb8e6f96
k <k>
2018-10-05 15:47:53 +0300

    test

---
+++ GradeBook/GradeBooks/StandartGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandartGradeBook : BaseGradeBook
+    {
+        public StandartGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
